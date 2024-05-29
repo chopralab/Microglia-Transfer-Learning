5075e05bb9f3cd9336ff5761fc9463a8f37d1c59
Jason Paul Finley <jpfinley@gilbreth-fe03.rcac.purdue.edu>
2024-05-29 11:56:49 -0400

    first commit

--- raytune.py
+++ raytune.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a YOLOv8n model
-model = YOLO('yolov8n.pt')
+model = YOLO('yolov8l.pt')
 
 # Start tuning hyperparameters for YOLOv8n training on the custom dataset
 result_grid = model.tune(data='custom.yaml', use_ray=True, epochs=30)
